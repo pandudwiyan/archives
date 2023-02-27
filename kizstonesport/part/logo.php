6b3424e638611c7cbd26b749038eccd30a37c4d0
Pandu Dwiyan W <33664470+pandudwiyan@users.noreply.github.com>
2023-02-27 18:35:52 +0700

    Delete kizstonesport directory

--- kizstonesport/part/logo.php
+++
@@ -1,5 +0,0 @@
-<div class="col-xl-2 col-6">
-                            <div class="header-logo">
-                                <a href="<?= $default_link; ?>"><img src="<?= $logo ?>" alt="Site Logo" style="height: 60px;" /></a>
-                            </div>
-                        </div>
