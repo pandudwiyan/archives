bbd3c98f6918c8bfb80adb4e45a36c9fbf774fc3
Pandu Dwiyan W <33664470+pandudwiyan@users.noreply.github.com>
2023-02-27 20:18:36 +0700

    Add files via upload

---
+++ kizstonesport/_setting/koneksi.php
@@ -0,0 +1,9 @@
+<?php 
+$host = "monza.id.domainesia.com";
+$username = "kizstone_admin";
+$password = "YB-IDg&PDJHr";
+$db_name = "kizstone_db";
+
+
+$koneksi = mysqli_connect($host,$username,$password,$db_name);
+?>
